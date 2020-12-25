3c25f0fa41f28fdc8ccc77c1390b573de16c9488
Olli-Pekka <ollipekkanikka@gmail.com>
2020-12-25 12:21:28 +0200

    Added logic to find traveler destinations and convert to internal data

--- script.py
+++ script.py
@@ -1,1 +1,24 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+# Traveller Erin Wilkes
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+### BOREDLESS TOURIST'S FUNCTIONALITY
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+
+# print("Destination index is: " + str(get_destination_index("Hyderabad, India"))) ## GIVES AN ValueError
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+# print("Travelers destination index: " + str(get_traveler_location(test_traveler))) ## Prints dindex
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
+
+
+
